a3a47833bbf84d34a210ac7c29f6d224b749f228
Tim Yu <tim.yu@opennet.tw>
2023-08-11 20:38:25 +0800

    Change seconds

--- myscript.sh
+++ myscript.sh
@@ -2,5 +2,5 @@
 
 # EHCO Command
 echo "Starting the process..."
-sleep 180  # Sleep for 3 minutes (180 seconds)
+sleep 60  # Sleep for 3 minutes (180 seconds)
 echo "Process completed after 3 minutes."
