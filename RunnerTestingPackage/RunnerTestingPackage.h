991f4c6b1dbb89e60a9739f12d67c336c1cc2f34
Tim Yu <tim.yu@opennet.tw>
2023-08-11 20:08:31 +0800

    Add a XCode project and test to it

---
+++ RunnerTestingPackage/RunnerTestingPackage.h
@@ -0,0 +1,18 @@
+//
+//  RunnerTestingPackage.h
+//  RunnerTestingPackage
+//
+//  Created by Tim on 2023/8/11.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RunnerTestingPackage.
+FOUNDATION_EXPORT double RunnerTestingPackageVersionNumber;
+
+//! Project version string for RunnerTestingPackage.
+FOUNDATION_EXPORT const unsigned char RunnerTestingPackageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RunnerTestingPackage/PublicHeader.h>
+
+
